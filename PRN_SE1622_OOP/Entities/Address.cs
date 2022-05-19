89841fbf0bb7023f35aec082c07420cecb419d04
DONGBHPC\Dong Bui Huu <buihuudong19@gmail.com>
2022-05-19 16:38:38 +0700

    Them moi chuc nang

---
+++ PRN_SE1622_OOP/Entities/Address.cs
@@ -0,0 +1,22 @@
+﻿namespace Prn.Se1622;
+
+public class Address
+{
+    public int No { get; set; }
+    public string Ward { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public Address() { }
+    public Address(int no, string ward, string street, string city)
+    {
+        No = no;
+        Ward = ward;
+        Street = street;
+        City = city;
+    }
+
+    public override string? ToString()
+    {
+        return $"[No = {No}, Ward = {Ward}, Street = {Street}, City ={City}]";
+    }
+}
--- PRN_SE1622_OOP/Program.cs
+++ PRN_SE1622_OOP/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace Prn.Se1622;
+using static System.Console;
+public class Program
+{
+    public static void Main()
+    {
+       /*
+        Student s = new Student { Id=1};
+       
+        s.Name = "Nguyen Van A";
+        s.Address = "Ha Noi";
+        s.Mark = 8.5d;
+        s.Dob = new DateOnly();
+       */
+        /*
+        Student a = new Student() { Id=10};
+        a.Name = "Dong";
+        a.Mark = 8.5d;
+        a.Address = "HN";
+        WriteLine(a);
+        
+        WriteLine("Hello");
+        */
+
+        Employee e = new Employee(1,"Dong", new Address(493,"Phuong 13","CMT8","HCM"),new DateTime(),150.66d,"Fresher");
+
+        WriteLine(e);
+
+        ReadLine();
+    }
+}
