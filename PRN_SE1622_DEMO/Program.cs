bfbf2c5b37ffe7028138c8f156213f45b246fc55
DONGBHPC\Dong Bui Huu <buihuudong19@gmail.com>
2022-05-19 15:01:19 +0700

    Add project files.

---
+++ PRN_SE1622_DEMO/Program.cs
@@ -0,0 +1,12 @@
+﻿Main();
+static void Main()
+{
+    int x = 1, y = 2;
+    void ShowValue( int a)
+    {
+        Console.WriteLine($"x = {x}, a={a}");
+        a = 10;
+    }
+    ShowValue(x);
+    Console.WriteLine(x);
+}
---
+++ PRN_SE1622_PRO/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace fptedu.se1622;//.NET 6
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"Hello, World....");
+
+        //declare more variables
+        Console.WriteLine($"Enter values for x and y: ");
+        
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+
+        int name;
+        
+        var salary = 100.5;//System.Double (CTS) double 
+        Console.WriteLine(salary.GetType().Name);
+
+        dynamic age = 20; //int32
+        age = true; //bool
+       
+
+        Console.WriteLine($"Total of a ={x} and b = {y} is: {Sum(x,y)}");
+        
+        Console.WriteLine(123_456_789 * 10);
+        
+        Console.ReadLine(); 
+    }
+
+    public static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+
+
